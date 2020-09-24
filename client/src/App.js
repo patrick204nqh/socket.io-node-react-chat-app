0c79ce01250d4e40d0220f066b10d7f95c38204a
patrick <patrick204nqh@gmail.com>
2020-09-25 01:41:07 +0700

    setup client & server

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { } from 'react-router-dom'
+
+function App() {
+  return (
+    <div>
+      hello, world
+    </div>
+  );
+}
+
+export default App;
